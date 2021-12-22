7d4bbbb060fa45c889c0766ada4d272875232917
Druv21 <dhruvag911@gmail.com>
2021-12-22 20:18:25 +0530

    improved css

--- src/services/api.js
+++ src/services/api.js
@@ -6,14 +6,4 @@ const apiKey="efccc4ae01fa6ad8739c96da2b9f7a2e";
 export const fetchApi=async(name)=>{
     const res=await axios.get(baseUrl+`q=${name}&units=metric&appid=${apiKey}`);
     return res;
-}
-
-// const resJson=await res.json();
-    // console.log(res);
-    // setCity(res.main);
-
-//       const response=await fetch(data);
-//       const resJson=await response.json();
-//       console.log(resJson);
-//       setCity(resJson.main);
-//     } 
+} 
