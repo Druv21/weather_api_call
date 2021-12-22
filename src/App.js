c877c6792b007d6ca395efe5d589035e35a6e802
Druv21 <dhruvag911@gmail.com>
2021-12-22 19:41:23 +0530

    used axios

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-const baseUrl="https://api.openweathermap.org/data/2.5/weather?";
-const apiKey="2abe78ccdc3580d00b9cb312be79c29c";
-function App() {
+import {fetchApi} from './services/api';
 
+function App() {
   const[city,setCity]=useState(null);
-  const[search,setSearch]=useState("Delhi");
-  const fetchApi=async()=>{
-    const data=baseUrl+`q=${search}&units=metric&appid=${apiKey}`;
-    const response=await fetch(data);
-    const resJson=await response.json();
-    console.log(resJson);
-    setCity(resJson.main);
-  } 
-    const getData=async()=>{
+  const[input,setInput]=useState("Delhi");
+
+  const getData=async()=>{
     try{
-      const data=await fetchApi();
-      console.log(data);
-    }catch(error){
-      console.log(error.message);
+         const resdata= await fetchApi(input);
+         console.log(resdata);
+         const actual= resdata.data;
+         setCity(actual.main);
+         console.log({city});
+         //return resdata;
+       }catch(error){
+    console.log(error.message);
     }
   }
+
   useEffect(() => {
-    fetchApi();
+    getData();
   },[])
- return(
+
+  return(
    <div className="App">
      <div className="card">
        <h2 className="title"><i className="fa fa-cloud"></i>Weather App</h2>
        <div className="search-form"> 
-         <input type="text" placeholder="Enter your city name" value={search} onChange={(event)=>{setSearch(event.target.value)}}/>
+         <input type="text" placeholder="Enter your city name" value={input} onChange={(event)=>{setInput(event.target.value)}}/>
          <button type="button" onClick={()=>getData()}>Search</button>
        </div>
        <div className="main-container">
@@ -40,7 +39,7 @@ function App() {
               <h1>{city.temp}&deg;C</h1>
             </div>
             <div className="location">
-              <h3><i className="fa fa-street-view"></i>{search}</h3>
+              <h3><i className="fa fa-street-view"></i>{input}</h3>
             </div>
             <div className="temperature-range">
               <h6>Min: {city.temp_min}&deg;C || Max: {city.temp_max}&deg;C || Humidity: {city.humidity}%</h6>
